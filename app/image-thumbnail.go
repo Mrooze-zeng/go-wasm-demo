7c112b181edda49faad20411b623f7acdb33e223
John <mrooze@v-hiker.cn>
2021-07-14 18:48:00 +0800

    生成图片缩略图

--- app/image-thumbnail.go
+++ app/image-thumbnail.go
@@ -11,17 +11,25 @@ import (
 func ImageThumbnail() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		buf := getBuffer(args)
-		r := bytes.NewBuffer(buf)
+		r := bytes.NewReader(buf)
+		// img, _, err := image.Decode(r)
+		// if err != nil {
+		// 	return nil
+		// }
+
+		// image := resize.Resize(160, 0, img, resize.Lanczos3)
 		srcImage, err := imaging.Decode(r)
 
 		if err != nil {
 			return nil
 		}
 
-		image := imaging.Resize(srcImage, 100, 0, imaging.Lanczos)
+		image := imaging.Resize(srcImage, 250, 0, imaging.Lanczos)
 
 		result := new(bytes.Buffer)
+
 		err = jpeg.Encode(result, image, nil)
+
 		if err != nil {
 			return nil
 		}
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0 // indirect
 	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 )
